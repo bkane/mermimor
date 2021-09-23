f510212f87152dffc4499adcd5cb38196276ad12
bkane <benjaminkane@gmail.com>
2021-09-23 09:06:46 -0400

    Fix vertical padding in standings entry and mute text for eliminated picks.

--- src/components/StandingsEntry.vue
+++ src/components/StandingsEntry.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="row bg-dark text-light">
         <div class="col-1 h4 text-center align-self-center py-0 px-1 fs-5">#{{ rank }}</div>
-        <div class="col h4 align-self-center py-0">
+        <div class="col h4 align-self-center pt-1 pb-0">
             <div class="vstack p-0">
                 {{ name }}
                 <p class="text-muted m-0 fs-6">{{ owner }}</p>
             </div>
         </div>
-        <div class="col-2 h4 ps-0 text-start align-self-center">{{ score }} pts</div>
+        <div class="col-2 h4 ps-0 text-start align-self-center pt-1">{{ score }} pts</div>
     </div>
 
     <div class="picks-table">
         <div class="row striped-row" v-for="pick in picks" :key="pick.id">
             <div class="col-1"></div>
-            <div class="col-8 col-md py-0 ps-4" :class="{ 'text-decoration-line-through': pick.eliminated }">
+            <div class="col-8 col-md py-0 ps-4" :class="{ 'text-decoration-line-through text-muted': pick.eliminated }">
                 {{ pick.name }}
             </div>
             <div class="col p-0 pe-5 text-start">{{ pick.points }}</div>
